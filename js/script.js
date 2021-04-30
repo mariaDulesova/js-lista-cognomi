17ed436724a741faa1865cf3ba6befe39df415f8
mariaDulesova <maria.dulesova@gmail.com>
2021-04-30 15:39:04 +0200

    update2

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ cognomiArray.push(cognomeUtente);
 console.log(cognomiArray);
 
 // 3. stampa la lista ordinata alfabeticamente
-var cognomiArray = cognomiArray.sort();
+cognomiArray = cognomiArray.sort();
 console.log(cognomiArray);
 
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
